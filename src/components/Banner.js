84acdc47d156e980e98322fa1db0d11bfe8956e5
onyangoouma <jacksonouma04@gmail.com>
2022-09-20 12:18:13 +0300

    reviewed a logo in the banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,18 +10,7 @@ const Banner = () => {
      My REAL ESTATE
      <ul class="nav-links">
        
-       <input type="checkbox" id="checkbox_toggle" />
-       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-       
-       <div class="menu">
-       <Link to="update">List</Link>
-       <Link to="about">About</Link>
-       <Link to="services" className='services'>Services</Link>
-       </div>
-     </ul>
-   </nav>
-    </div>
-  )
+      
 }
 
 export default Banner
