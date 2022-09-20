0f0b20d472d2cbfd7fa8b55e4b0d106cc26f88b3
onyangoouma <jacksonouma04@gmail.com>
2022-09-20 12:25:31 +0300

    added custom form, update route in the main component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ import Update from './components/Update'
 import Home from './components/pages/Home'
 
 
-
+const App = () => {
+  return (
+    <div className='max-w-[1440px] mx-auto bg-white'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/customform' element={<CustomForm />} />
+        <Route path='/update' element={<Update/>} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
