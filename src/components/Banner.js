d99ce3885db3aba022f852ae13a09f211139849a
onyangoouma <jacksonouma04@gmail.com>
2022-09-20 12:19:15 +0300

    added linked list to the banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,18 @@ const Banner = () => {
      My REAL ESTATE
      <ul class="nav-links">
        
-      
+       <input type="checkbox" id="checkbox_toggle" />
+       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+       
+       <div class="menu">
+       <Link to="update">List</Link>
+       <Link to="about">About</Link>
+       <Link to="services" className='services'>Services</Link>
+       </div>
+     </ul>
+   </nav>
+    </div>
+  )
 }
 
 export default Banner
